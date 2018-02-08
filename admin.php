0abc22ea73eab421b0672c01f544d92827d4cdb3
Pavel Radchuk <pradchuk2@mail.greenriver.edu>
2018-02-08 14:20:25 -0500

    added admin

---
+++ admin.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: BBTL
+ * Date: 2/8/18
+ * Time: 11:12 AM
+ */
+class admin
+{
+    private $_username;
+    private $_password;
+    private $_initializes;
+    private $_accessLevel;
+
+
+    function __construct()
+    {
+        $this->_username = "";
+        $this->_password = "";
+        $this->_accessLevel = "";
+        $this->_initializes = "";
+    }
+    function getAccessLevel(){
+        return $this->_accessLevel;
+    }
+    function setAccessLevel(){
+        return $this->_accessLevel;
+    }
+    function Admin($editUser){
+        
+    }
+}
